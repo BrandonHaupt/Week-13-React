d1dd6b1e4af9f1b0ef4b0107a2f8353595ef1a41
Brandon Haupt <haupt.brandon@gmail.com>
2022-12-14 18:59:25 -0600

    Update App.js
    - testing Components and JSX
    - JSX is the HTML
    - Components are the functions

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// JSX is the HTML and the Function is the Component 
+function Heading(){
+  return(
+    <>
+      <h1>Hello World!</h1>
+    </>
+  )
+}
+
+
+function App(){
+  // <></> <--- this is called a Fragment | Allows us to write multiple html elements
+  return <>
+    <Heading />
+  </>
 }
 
 export default App;
